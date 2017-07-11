a037ee9f5f6918571603e77c9b0eca254ab6c07b
Filippo Valle <filippo.valle@edu.unito.it>
2017-07-11 11:44:23 +0200

    add class Translator

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from translate import translate_text
+import pandas as pd
+from translate import TranslationManager as tr
 
+#paste here your key
+#get a key from https://cloud.google.com/translate/docs/getting-started
+translator=tr('')
 
-first_translation=translate_text('en','Finito')
+first_translation=translator.translate_text('en','Finito')
 print first_translation
-second_translation=translate_text('ja',first_translation)
+second_translation=translator.translate_text('ja',first_translation)
 print second_translation
+
+languages=['it','fr','es','de','en','ru','am','co','cy','da']
+words=['gatto','world']
+
+translated=[]
+previous=''
+
+for word in words:
+    previous=translator.translate_text('it',word)
+    for lang in languages:
+        previous=translator.translate_text(lang,previous)
+        print previous, translator.translate_text(lang,word)
--- translate.py
+++ translate.py
@@ -1,13 +1,23 @@
 import requests #API
 
-
-def translate_text(target, text):
-    #update key
-    key=''
-    url='https://translation.googleapis.com/language/translate/v2'
-
-    payload={'key':key, 'target':target, 'q':text}
-    result=requests.get(url, params=payload)
-    data=result.json()
-    toReturn= data['data']['translations'][0]['translatedText']
-    return toReturn
+class TranslationManager(object):
+    """docstring for TranslationManager."""
+    def __init__(self):
+        super(TranslationManager, self).__init__()
+        self.key=''
+    
+    def __init__(self, key):
+        super(TranslationManager,self).__init__()
+        self.key=key
+    
+    def SetKey(key):
+        self.key=key
+    
+    def translate_text(self, target, text):
+        url='https://translation.googleapis.com/language/translate/v2'
+        
+        payload={'key':self.key, 'target':target, 'q':text}
+        result=requests.get(url, params=payload)
+        data=result.json()
+        toReturn= data['data']['translations'][0]['translatedText']
+        return toReturn
